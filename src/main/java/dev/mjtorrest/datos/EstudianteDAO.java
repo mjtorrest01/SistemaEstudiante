7ffe64d314865574d412c28095038b3113838378
Michael Torres <michaeljtt@outlook.com>
2023-10-21 17:29:22 -0500

    Creando funcion de buscar estudiantes

--- src/main/java/dev/mjtorrest/datos/EstudianteDAO.java
+++ src/main/java/dev/mjtorrest/datos/EstudianteDAO.java
@@ -2,9 +2,11 @@ package dev.mjtorrest.datos;
 
 import dev.mjtorrest.dominio.Estudiante;
 
+import javax.xml.transform.Result;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,17 +39,56 @@ public class EstudianteDAO {
             try {
                 con.close();
             } catch (Exception e){
-                System.out.println("Ocurrio un error al cerrar coneccion");
+                System.out.println("Ocurrio un error al cerrar coneccion " + e.getMessage());
             }
         }
         return estudiantes;
     }
 
+    // findById
+    public boolean buscarEstudiantePorId(Estudiante estudiante){
+        PreparedStatement ps;
+        ResultSet rs;
+        Connection con = getConexion();
+        String sql = "SELECT * FROM estudiante WHERE id_estudiante = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, estudiante.getIdEstudiante());
+            rs = ps.executeQuery();
+            if(rs.next()){
+                estudiante.setNombre(rs.getString("nombre"));
+                estudiante.setApellido(rs.getString("apellido"));
+                estudiante.setTelefono(rs.getString("telefono"));
+                estudiante.setEmail(rs.getString("email"));
+                return true;
+            }
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error al buscar estudiante");
+        }
+        finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Ocurrio un error al cerrar coneccion " + e.getMessage());
+            }
+        }
+        return false;
+    }
+
     // MAIN DE PRUEBA DE LISTAR ESTUDIANTES
     public static void main(String[] args) {
         var estudianteDao = new EstudianteDAO();
-        System.out.println("Listar Estudiantes");
-        List<Estudiante> estudiantes = estudianteDao.listarEstudiantes();
-        estudiantes.forEach(System.out::println);
+        //System.out.println("Listar Estudiantes");
+        //List<Estudiante> estudiantes = estudianteDao.listarEstudiantes();
+        //estudiantes.forEach(System.out::println);
+
+        // BUSCAR POR ID
+        var estudiante1 = new Estudiante(3);
+        System.out.println("Estudiante antes de la busqueda: " + estudiante1);
+        var encontrado = estudianteDao.buscarEstudiantePorId(estudiante1);
+        if(encontrado)
+            System.out.println("Estudiante encontrado: " + estudiante1);
+        else
+            System.out.println("No se encontro estudiante: " + estudiante1.getIdEstudiante());
     }
 }
